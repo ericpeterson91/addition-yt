205ecac33411246924513f14190aa4dcaaa00cf3
Eric Peterson <ericpeterson91@live.ca>
2022-11-26 17:25:32 -0700

    format

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let container = document.querySelector('.container')
 let num1 = document.querySelector(".num1");
 let num2 = document.querySelector(".num2");
 let input = document.querySelector("input");
+let score = document.querySelector(".score");
+let count = 0;
 
 num1.innerHTML = Math.ceil(Math.random() * 10);
 num2.innerHTML = Math.ceil(Math.random() * 10);
 
-let score = document.querySelector(".score");
-let count = 0;
 
 input.addEventListener("keypress", (event) => {
-    
+
   if (event.key === "Enter") {
     let sum = parseInt(num1.innerHTML) + parseInt(num2.innerHTML);
     let response = parseInt(input.value);
